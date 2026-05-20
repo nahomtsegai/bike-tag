a62f0fa75cb9be68a6e9c49fedace57f82d7196b
ntsegai <nahom.tsegai@morningstar.com>
2026-05-19 22:24:18 -0500

    Add current tag API

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  css: ['~/assets/css/main.css']
+
+  runtimeConfig: {
+    supabaseUrl: '',
+    supabaseServiceRoleKey: '',
+    supabaseStorageBucket: 'bike_tag_photos',
+    public: {
+      supabaseAnonKey: ''
+    }
+  }
 })
