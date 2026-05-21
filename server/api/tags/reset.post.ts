95cecff5ade13cae0ea8141c9d71e8a9a19a7d65
ntsegai <nahom.tsegai@morningstar.com>
2026-05-21 13:07:21 -0500

    Guard reset API

--- server/api/tags/reset.post.ts
+++ server/api/tags/reset.post.ts
@@ -1,16 +1,39 @@
+import { getTagDataSource } from '../../utils/tagDataSource'
+import { resetTagStore } from '../../utils/tagStore'
 import {
   createCurrentTagResponse,
   createFoundTagResponse
 } from '../../utils/tagResponse'
-import { resetTagStore } from '../../utils/tagStore'
+
+const isDevelopmentEnvironment = () => {
+  return import.meta.dev
+}
+
+const assertResetIsAllowed = () => {
+  if (!isDevelopmentEnvironment()) {
+    throw createError({
+      statusCode: 403,
+      statusMessage: 'Reset is only available in development.'
+    })
+  }
+
+  if (getTagDataSource() !== 'mock') {
+    throw createError({
+      statusCode: 403,
+      statusMessage: 'Reset is only available when using the mock data source.'
+    })
+  }
+}
 
 export default defineEventHandler(() => {
+  assertResetIsAllowed()
+
   const resetResult = resetTagStore()
 
   if (!resetResult.currentTag) {
     throw createError({
       statusCode: 500,
-      statusMessage: 'Mock tag store reset did not create an active tag.'
+      statusMessage: 'Mock game data reset did not create an active tag.'
     })
   }
 
