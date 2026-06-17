19824c6f6229098a16e49931a13a5d9f630701b2
Nahom Tsegai <nytsegai@gmail.com>
2026-06-17 10:28:43 -0400

    feat: define active tag submission binding constants

---
+++ shared/utils/submitActiveTag.ts
@@ -0,0 +1,5 @@
+export const expectedActiveTagIdHeaderName =
+  'x-bike-tag-expected-active-tag-id'
+
+export const activeTagChangedStatusMessage =
+  'The active Bike Tag changed while you were preparing this submission. Review the new current tag before trying again.'
