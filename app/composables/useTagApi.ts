90d38f8fefce858dcc447269531be83b5a997305
Nahom Tsegai <nytsegai@gmail.com>
2026-06-17 11:02:27 -0400

    refactor: keep active tag binding in request header

--- app/composables/useTagApi.ts
+++ app/composables/useTagApi.ts
@@ -97,8 +97,6 @@ export const useTagApi = () => {
       throw createMissingExpectedActiveTagError()
     }
 
-    input.set('expectedActiveTagId', expectedActiveTagId)
-
     return await $fetch<SubmitTagApiResponse>('/api/tags/submit', {
       method: 'POST',
       body: input,
