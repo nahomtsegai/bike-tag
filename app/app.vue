e71622b471b2c13d8abe557f36ad4c562643c0f7
Nahom Tsegai <nytsegai@gmail.com>
2026-06-18 23:27:42 -0400

    add global skip navigation link

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtPage />
+
+    <a class="skipLink" href="#main-content">
+      Skip to main content
+    </a>
+
+    <div id="main-content" tabindex="-1">
+      <NuxtPage />
+    </div>
+
     <AppFooter />
   </div>
 </template>
